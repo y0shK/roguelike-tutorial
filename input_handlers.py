bf97317619d3580b4f229cb06d93f68ee909dccb
Yash Karandikar <25213173+y0shK@users.noreply.github.com>
2020-01-05 14:06:49 -0800

    Create input_handlers.py

---
+++ input_handlers.py
@@ -0,0 +1,23 @@
+import tcod as libtcod
+
+def handle_keys(key):
+    # set movement keys
+    if key.vk == libtcod.KEY_UP:
+        return {'move': (0,-1)}
+    elif key.vk == libtcod.KEY_DOWN:
+        return {'move': (0,1)}
+    elif key.vk == libtcod.KEY_LEFT:
+        return {'move': (-1,0)}
+    elif key.vk == libtcod.KEY_RIGHT:
+        return {'move': (1,0)}
+
+    if key.vk == libtcod.KEY_ENTER and key.lalt or key.ralt:
+        # alt+enter - fullscreen
+        return {'fullscreen': True}
+    elif key.vk == libtcod.KEY_ESCAPE:
+        # exit the game
+        return {'exit': True}
+
+    # no key press?
+    return {}
+    
